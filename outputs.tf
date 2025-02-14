67ef1ce93eae94f5be42ac1fd077459e566bc590
malmousawi <96029001+malmousawi@users.noreply.github.com>
2025-02-14 08:42:02 +0000

    terraform changes

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,15 @@
 
+
+output "host_vm_ips" {
+  value = harvester_virtualmachine.host[*].network_interface[0].ip_address
+}
+
+output "host_vm_ids" {
+  value = harvester_virtualmachine.host[*].id
+}
+
+
+
 output "worker_vm_ips" {
   value = harvester_virtualmachine.worker[*].network_interface[0].ip_address
 }
